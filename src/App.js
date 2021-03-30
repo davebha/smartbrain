4efc627b6100ece0fa78a2e7060f3230f34f6cc3
Bharat Dave <davebharat93@gmail.com>
2021-03-30 19:02:57 -0400

    Create basic skeleton for navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation/navigation'
 
 function App() {
   return (
     <div>  
-    {/*<Navigation />
-    <Logo />
+    <Navigation />
+    {/*<Logo />
     Rank
     ImageLinkForm
        FaceRecognition*/}
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,9 +1,13 @@
-import React from './react'
+import React from 'react'
 
 const navigation = ()=>{
-
-
-
+	return(
+		<div>
+			<nav>
+				<p>Sign out</p>
+			</nav>
+		</div>
+		);
 }
 
 export default navigation;
