d2e2cd3ce7cf272308a401e86fb68e468e20029d
Bharat Dave <davebharat93@gmail.com>
2021-04-01 11:22:36 -0400

    Create logo component module

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div>  
     <Navigation />
-    {/*<Logo />
+    <Logo />
+    {/*
     Rank
     ImageLinkForm
        FaceRecognition*/}
---
+++ src/components/logo/logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const logo =()=>{
+	return(
+		<div>
+			
+		</div>
+		);
+
+}
+
+export default logo;
