d27a79c45443e6f41cbdc89960502c92817d5801
Bharat Dave <davebharat93@gmail.com>
2021-04-02 17:08:42 -0400

    Add brain image and padding for the icon

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt'
 import './logo.css'
+import brain from './brain.png'
 
 const logo =()=>{
 	return(
 		<div className='ma6 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner"> 
- 				<img src="brain.png" alt="logo" style={{padding-top:'55px'}} </div>
+			<Tilt className="Tilt br2 shadow-2 " options={{ max : 55 }} style={{ height: 140, width: 140 }} >
+ 				<div className="Tilt-inner pa4"> <img src={brain} alt="logo" style={{paddingTop:'5px'}} />  </div>
 			</Tilt>
 		</div>
 		);
