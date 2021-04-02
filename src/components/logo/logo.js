bb0fdb25e5d5a50c05daa80937508bd810afac78
Bharat Dave <davebharat93@gmail.com>
2021-04-02 16:12:56 -0400

    Add background for default alien icon and the page

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Tilt from 'react-tilt'
+import './logo.css'
 
 const logo =()=>{
 	return(
-		<div>
-			
+		<div className='ma6 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+ 				<div className="Tilt-inner"> 
+ 				<img src="brain.png" alt="logo" style={{padding-top:'55px'}} </div>
+			</Tilt>
 		</div>
 		);
 
