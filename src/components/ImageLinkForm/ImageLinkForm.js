192edf6ad1c2b948778e29b1fc75c683bae0d473
Bharat Dave <davebharat93@gmail.com>
2021-04-07 15:59:04 -0400

    Center the searchbox and button and add pattern to it

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import './ImageLinkForm.css'
 
 const ImageLinkForm =()=>{
 	return(
 		<div>
-			<p className='f3'>
+			<p className='f3 center'>
 				{'This magical brain will detect faces in your photos.Give it a try!'}
 			</p>
-			<div >
-				<input className='f4 pa2 w-70' type='text' />
-				<button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>Detect</button>
+			<div className='center'>
+				<div className='form  pa4 br3 shadow-5 center '>
+					<input className='f4 pa2 w-70 center' type='text' />
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+				</div>
 			</div>
 		</div>
 		);
