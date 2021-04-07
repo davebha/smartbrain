d5e1e42eb26ae499d3bef81b6282875a54bc8d7a
Bharat Dave <davebharat93@gmail.com>
2021-04-07 15:22:54 -0400

    Move the searchfield and detect button on single line

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,12 +4,12 @@ import React from 'react';
 const ImageLinkForm =()=>{
 	return(
 		<div>
-			<p>
+			<p className='f3'>
 				{'This magical brain will detect faces in your photos.Give it a try!'}
 			</p>
-			<div className='bgCenter'>
-				<input type='text' />
-				<button>Detect</button>
+			<div >
+				<input className='f4 pa2 w-70' type='text' />
+				<button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>Detect</button>
 			</div>
 		</div>
 		);
