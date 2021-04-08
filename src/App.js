d2921423741d42ac30aa8113ed554a37633fd4c3
Bharat Dave <davebharat93@gmail.com>
2021-04-08 15:04:21 -0400

    Create and reference Rank component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Navigation from './components/navigation/navigation'
 import Logo from './components/logo/logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 function App() {
   return (
-    <div>  
-    <Navigation />
-    <Logo />
-    <ImageLinkForm/>
+    <div className='App'>  
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm/>
     {/*
-    Rank
-    ImageLinkForm
+    
        FaceRecognition*/}
     </div>
   );
