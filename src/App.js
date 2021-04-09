31d085719161d5ef50ee388e56f755ad16332a84
Bharat Dave <davebharat93@gmail.com>
2021-04-09 04:53:01 -0400

    Integrate particles library in the app background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import React,{Component} from 'react'
 import Navigation from './components/navigation/navigation'
 import Logo from './components/logo/logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
-function App() {
+import Particles from 'react-particles-js'
+
+  const particlesOptions={
+                particles: {
+                  line_linked: {
+                    shadow: {
+                      enable: true,
+                      color: "#3CA9D1",
+                      blur: 5
+                    }
+                  }
+                }
+              }
+
+class App extends Component{
+
+  render(){
   return (
     <div className='App'>  
+      <Particles 
+                params={particlesOptions} className='particles' />
       <Navigation />
       <Logo />
       <Rank />
@@ -15,6 +34,8 @@ function App() {
        FaceRecognition*/}
     </div>
   );
+}
+
 }
 
 export default App;
