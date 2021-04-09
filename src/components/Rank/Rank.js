d2ab40e6fdfd0b6b79e337ce509bb9b244f40a7f
Bharat Dave <davebharat93@gmail.com>
2021-04-09 03:41:20 -0400

    Add rank feedback

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Rank =()=>{
 	return(
 		<div>
-		
+			<div className='white f3'>
+			{'Bharat, your current rank is ...'}
+			</div>
+			<div className='white f2'>
+			{'#5'}
+			</div>
 		</div>
 		);
 
