d35a8a6419f8cfbbaeabd5b723317e33ffe4e042
Bharat Dave <davebharat93@gmail.com>
2021-04-14 17:55:57 -0400

    Display detect button feedback on click

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ class App extends Component{
   onInputChange=(event)=>{
     console.log(event.target.value)
   }
+
+  onButtonSubmit = ()=>{
+    console.log('click!');
+  }
   render(){
   return (
     <div className='App'>  
@@ -38,7 +42,8 @@ class App extends Component{
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm   onInputChange={this.onInputChange}/>
+      <ImageLinkForm   onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
     {/*
     
        FaceRecognition*/}
