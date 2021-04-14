179ab90043584e05af5e8596d1e03a19ebe28709
Bharat Dave <davebharat93@gmail.com>
2021-04-14 17:51:36 -0400

    Display input box text to the console

--- src/App.js
+++ src/App.js
@@ -20,6 +20,16 @@ import Particles from 'react-particles-js'
 
 class App extends Component{
 
+  constructor(){
+    super();
+    this.state={
+      input:''
+    }
+  }
+
+  onInputChange=(event)=>{
+    console.log(event.target.value)
+  }
   render(){
   return (
     <div className='App'>  
@@ -28,7 +38,7 @@ class App extends Component{
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm/>
+      <ImageLinkForm   onInputChange={this.onInputChange}/>
     {/*
     
        FaceRecognition*/}
